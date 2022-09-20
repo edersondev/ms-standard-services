1b0ac27f832796548edc7560bb4ec8d5d01835f1
Ederson Ferreira <ederson.dev@gmail.com>
2022-09-20 19:59:57 -0300

    feat(model): setting up model of country

---
+++ app/Models/Country.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tb_country';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'iso_code',
+        'iso_code3',
+        'number_code',
+        'dial'
+    ];
+}
