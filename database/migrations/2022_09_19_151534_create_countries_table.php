2262eb8ce4b1a46a207f09ef464faf7d9c794890
Ederson Ferreira <ederson.dev@gmail.com>
2022-09-20 11:02:14 -0300

    feat(migration): define columns of table country

---
+++ database/migrations/2022_09_19_151534_create_countries_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_country', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 80)
+                ->comment('Country name');
+
+            $table->string('iso_code', 2)
+                ->comment('Alpha-2 codes are two-letter country codes defined in ISO 3166-1');
+
+            $table->string('iso_code3', 3)
+                ->comment('Alpha-3 codes are three-letter country codes defined in ISO 3166-1');
+
+            $table->smallInteger('number_code')
+                ->comment('Numeric (or numeric-3) codes are three-digit country codes defined in ISO 3166-1');
+
+            $table->integer('dial')
+                ->comment('International telephone dialing codes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_country');
+    }
+};
