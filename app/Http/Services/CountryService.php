ccd7180f1e07836900cd534b4eba80438ca4332d
Ederson Ferreira <ederson.dev@gmail.com>
2022-10-02 00:56:07 -0300

    feat(repository): update service to get repository

--- app/Http/Services/CountryService.php
+++ app/Http/Services/CountryService.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Services;
 
-use App\Models\Country;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Builder;
+use App\Repositories\CountryRepository;
 
 class CountryService
 {
     /**
-     * @var Country
+     * @var CountryRepository
      */
-    protected $_entity;
+    protected $_repository;
 
-    public function __construct(Country $entity)
+    public function __construct(CountryRepository $repository)
     {
-        $this->_entity = $entity;
+        $this->_repository = $repository;
     }
 
     /**
@@ -28,34 +27,9 @@ class CountryService
         $per_page = ($request->has('per_page') ? $request->per_page : 10);
         if($per_page > 100) {$per_page = 100;}
 
-        /** @var Builder */
-        $obj = $this->_entity::orderBy('name');
-
-        $this->filterIndex($request, $obj);
-
-        return $obj->paginate($per_page);
+        return $this->_repository
+            ->index($request)
+            ->paginate($per_page);
     }
 
-    /**
-     * @param Request $request
-     * @param Builder $obj
-     */
-    public function filterIndex(Request $request, Builder &$obj): void
-    {
-        if($request->has('name') && !empty($request->name)){
-            $obj->where('name','like',"%{$request->name}%");
-        }
-
-        if($request->has('iso_code') && !empty($request->iso_code)){
-            $obj->where('iso_code', $request->iso_code);
-        }
-
-        if($request->has('iso_code3') && !empty($request->iso_code3)){
-            $obj->where('iso_code3', $request->iso_code3);
-        }
-
-        if($request->has('number_code') && !empty($request->number_code)){
-            $obj->where('number_code', $request->number_code);
-        }
-    }
 }
