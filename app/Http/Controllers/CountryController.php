88cc4ff95e7494afecca94caaebae740be38039c
Ederson Ferreira <ederson.dev@gmail.com>
2023-03-21 17:30:12 -0300

    docs: create description to api using scribe

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -15,6 +15,18 @@ class CountryController extends Controller
         $this->_service = $service;
     }
 
+    /**
+     * List of countries
+     *
+     * This endpoint will bring a list of countries around the World, you'll get a 200 OK response.
+     *
+     * @responseField id Unique identify
+     * @responseField name The country name
+     * @responseField iso_code Alpha-2 codes are two-letter country codes defined in ISO 3166-1
+     * @responseField iso_code3 Alpha-3 codes are three-letter country codes defined in ISO 3166-1
+     * @responseField number_code Numeric (or numeric-3) codes are three-digit country codes defined in ISO 3166-1
+     * @responseField dial International telephone dialing codes
+     */
     public function index(Request $request)
     {
         return new ResponseCollection($this->_service->index($request));
