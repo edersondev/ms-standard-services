c44f18aa24d5161aae63c23e202a67c3ed2e7669
Ederson Ferreira <ederson.dev@gmail.com>
2023-11-01 17:13:17 -0300

    test: list of regions

--- tests/Feature/RegionControllerTest.php
+++ tests/Feature/RegionControllerTest.php
@@ -3,18 +3,39 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use App\Models\Country;
+use App\Models\Region;
 use Tests\TestCase;
 
 class RegionControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+
+    protected $_end_point = '/api/regions';
+
+    /**
+     * @test
+     */
+    public function whenIndexThenReturnSuccess(): void
+    {
+        $country = Country::factory()->create();
+
+        $number_items = random_int(2, 5);
+
+        Region::factory($number_items)->create([
+            'country_id' => $country->id
+        ]);
+
+        $iso_code = strtolower($country->iso_code);
+
+        $this->getJson("{$this->_end_point}/{$iso_code}")
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('data', $number_items, fn ($json) =>
+                    $json->hasAll(['id', 'name', 'region_code', 'country_id'])
+                )
+            );
     }
 }
