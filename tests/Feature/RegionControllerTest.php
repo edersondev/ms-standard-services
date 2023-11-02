bcde4924ad54d6da52cd38d0b6bbfa4cfa043b8d
Ederson Ferreira <ederson.dev@gmail.com>
2023-11-02 11:54:04 -0300

    test: add when create and empty list

--- tests/Feature/RegionControllerTest.php
+++ tests/Feature/RegionControllerTest.php
@@ -15,6 +15,8 @@ class RegionControllerTest extends TestCase
 
     protected $_end_point = '/api/regions';
 
+    protected $_response_fields = ['id', 'name', 'region_code', 'country_id'];
+
     /**
      * @test
      */
@@ -30,11 +32,11 @@ class RegionControllerTest extends TestCase
 
         $iso_code = strtolower($country->iso_code);
 
-        $this->getJson("{$this->_end_point}/{$iso_code}")
+        $this->getJson("{$this->_end_point}?country_iso={$iso_code}")
             ->assertStatus(200)
             ->assertJson(fn (AssertableJson $json) =>
                 $json->has('data', $number_items, fn ($json) =>
-                    $json->hasAll(['id', 'name', 'region_code', 'country_id'])
+                    $json->hasAll($this->_response_fields)
                 )
             );
     }
@@ -48,10 +50,45 @@ class RegionControllerTest extends TestCase
 
         $iso_code = strtolower($country->iso_code);
 
-        $this->getJson("{$this->_end_point}/{$iso_code}")
+        $this->getJson("{$this->_end_point}?country_iso={$iso_code}")
             ->assertStatus(200)
             ->assertJson(fn (AssertableJson $json) =>
                 $json->has('data', 0)
+            );
+    }
+
+    /**
+     * @test
+     */
+    public function whenIndexWithoutCountryIsoThenReturnError(): void
+    {
+        $this->getJson("{$this->_end_point}")
+            ->assertStatus(422)
+            ->assertInvalid(['country_iso']);
+    }
+
+    /**
+     * @test
+     */
+    public function whenCreateThenReturnSuccess(): void
+    {
+        $country = Country::factory()->create();
+
+        $data_post = [
+            'name' => fake()->name(),
+            'region_code' => fake()->numberBetween(10,999),
+            'country_id' => $country->id
+        ];
+
+        $response = $this->postJson($this->_end_point, $data_post)
+            ->assertStatus(201);
+
+        $location = route('regions.show', ['region' => $response['id']]);
+
+        $response
+            ->assertHeader('location', $location)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->hasAll($this->_response_fields)
             );
     }
 }
